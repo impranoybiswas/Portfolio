fd3178a523cc13011ea5f30c9effdf58689c1527
Pranoy Biswas <impranoybiswas@gmail.com>
2025-07-25 11:37:05 +0600

    convert to nextjs ts

--- src/hooks/usePageTitle.jsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-export default function usePageTitle(title) {
-  useEffect(() => {
-    const siteName = "Website Title";
-    document.title = title ? `${title} | ${siteName}` : siteName;
-  }, [title]);
-}
