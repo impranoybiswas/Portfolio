4e1a4e5b31b88f171125a7b10deae9ff0eac2131
Pranoy Biswas <impranoybiswas@gmail.com>
2025-07-31 00:15:12 +0600

    loading change

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,8 @@ import React from "react";
 export default function Loading() {
   return (
     <main className="flex justify-center items-center h-screen w-full">
-      <Image className="animate-ping" src="/logo.svg" alt="loading" width={60} height={60} />
+      {/* <Image className="animate-ping" src="/logo.svg" alt="loading" width={60} height={60} /> */}
+      Loading....
     </main>
   );
 }
