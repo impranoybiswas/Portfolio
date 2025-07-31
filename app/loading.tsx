0ed1a3f215d9a0b39f74c64b13289fc85ef9cc59
Pranoy Biswas <impranoybiswas@gmail.com>
2025-07-31 11:42:41 +0600

    loading problem

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <main className="flex justify-center items-center h-screen w-full">
+    <main className="flex justify-center items-center h-screen w-full text-white">
       {/* <Image className="animate-ping" src="/logo.svg" alt="loading" width={60} height={60} /> */}
       Loading....
     </main>
